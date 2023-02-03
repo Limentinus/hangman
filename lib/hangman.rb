c587b4e586e366047919227cb5ba4f5d783b2310
LughsClues <cuberfreak@gmail.com>
2023-02-03 15:04:52 +0100

    Add check_game_over methods

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -2,6 +2,7 @@ class Hangman
   def initialize
     @words = File.readlines('google-10000-english-no-swears.txt')
     @word_to_guess
+    @guessed_letters = ''
   end
 
   def choose_random_word
@@ -10,8 +11,38 @@ class Hangman
     end.strip
   end
 
+  def display_game_state
+    @word_to_guess.chars.map do |c|
+      if @guessed_letters.include?(c)
+        c
+      else
+        "_"
+      end
+    end.join(" ") 
+  end
 
+  def check_game_over
+    @word_to_guess.chars.map do |c|
+      if @guessed_letters.length > 8
+        puts "You lose the word was #{@word_to_guess}"
+        return true
+      elsif @word_to_guess.chars.all? {|c| @guessed_letters.include?(c)}
+        puts "Congratulations, you won the game! The word was #{@word_to_guess}"
+        return true
+      end
+    end
+    false
+  end
+
+  def play_game
+    choose_random_word
+    puts "Its hangman. 
+    while true
+
+    Guess the word by typing in a letter or save the game by typing save. To exit, type exit."
+    puts display_game_state
+  end
   
 end
 
-Hangman.new
+Hangman.new.play_game 
